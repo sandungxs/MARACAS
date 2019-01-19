136ad08516555d84d22c4b4ac53df66e9b798c8f
Francisco J. Romero-Campero <fran@us.es>
2019-01-19 18:50:39 +0100

    Fix errors in ostta annotation due to gene/transcript id duplications and an alternative splicing with the same transcript id

--- scripts/sample_processing.sh
+++ scripts/sample_processing.sh
@@ -15,27 +15,27 @@ ANNOTATION=$4
 
 ## Downloading sample file
 cd ${SAMPLE_FOLDER} 
-fastq-dump --split-files ${ACC_NUMBER}
+#fastq-dump --split-files ${ACC_NUMBER}
 
 ## Sample quality control and read mapping to reference genome
-if [ -f ${ACC_NUMBER}_2.fastq ]
-then
-   fastqc ${ACC_NUMBER}_1.fastq
-   fastqc ${ACC_NUMBER}_2.fastq
+#if [ -f ${ACC_NUMBER}_2.fastq ]
+#then
+#   fastqc ${ACC_NUMBER}_1.fastq
+#   fastqc ${ACC_NUMBER}_2.fastq
 
-   hisat2 --dta -x $INDEX -1 ${ACC_NUMBER}_1.fastq -2 ${ACC_NUMBER}_2.fastq -S ${ACC_NUMBER}.sam
-else
-   fastqc ${ACC_NUMBER}_1.fastq
+#   hisat2 --dta -x $INDEX -1 ${ACC_NUMBER}_1.fastq -2 ${ACC_NUMBER}_2.fastq -S ${ACC_NUMBER}.sam
+#else
+#   fastqc ${ACC_NUMBER}_1.fastq
 
-   hisat2 --dta -x $INDEX -U ${ACC_NUMBER}_1.fastq -S ${ACC_NUMBER}.sam
-fi
+#   hisat2 --dta -x $INDEX -U ${ACC_NUMBER}_1.fastq -S ${ACC_NUMBER}.sam
+#fi
 
 ## Generting sorted bam file
-samtools sort -o ${ACC_NUMBER}.bam ${ACC_NUMBER}.sam
-rm ${ACC_NUMBER}.sam
-rm *.fastq
-samtools index ${ACC_NUMBER}.bam
-bamCoverage -bs 10 --normalizeUsing CPM --bam ${ACC_NUMBER}.bam -o ${ACC_NUMBER}.bw
+#samtools sort -o ${ACC_NUMBER}.bam ${ACC_NUMBER}.sam
+#rm ${ACC_NUMBER}.sam
+#rm *.fastq
+#samtools index ${ACC_NUMBER}.bam
+#bamCoverage -bs 10 --normalizeUsing CPM --bam ${ACC_NUMBER}.bam -o ${ACC_NUMBER}.bw
 
 ## Transcript assembly
 stringtie -G $ANNOTATION -o ${ACC_NUMBER}.gtf -l ${ACC_NUMBER} ${ACC_NUMBER}.bam
