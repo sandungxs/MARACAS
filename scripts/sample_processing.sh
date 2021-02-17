7728d7fe75f1b5bae855f8be54b8a206ff739768
Francisco J. Romero-Campero <fran@us.es>
2021-02-17 15:00:04 +0100

    Add sequential sample processing in a single server

--- scripts/sample_processing.sh
+++ scripts/sample_processing.sh
@@ -2,13 +2,9 @@
 ##          Ana Belen Romero-Losada
 ## Contact: Francisco J. Romero-Campero - fran@us.es
 
-#$ -S /bin/bash
-#$ -V
-#$ -cwd
-#$ -j yes
+#! /bin/bash
 
 ## Name input parameters
-
 DATA=$1
 PAIRED=$2
 SAMPLE_FOLDER=$3
@@ -23,6 +19,7 @@ FOLD_CHANGE=${11}
 Q_VALUE=${12}
 FASTQ_LEFT=${13}
 FASTQ_RIGHT=${14}
+NPROC=${15}
 
 ACC_NUMBER=${FASTQ_LEFT}
 
@@ -51,40 +48,26 @@ then
    fastqc ${ACC_NUMBER}_1.fastq
    fastqc ${ACC_NUMBER}_2.fastq
 
-   hisat2 --dta -x $INDEX -1 ${ACC_NUMBER}_1.fastq -2 ${ACC_NUMBER}_2.fastq -S ${ACC_NUMBER}.sam
+   hisat2 -p $NPROC --dta -x $INDEX -1 ${ACC_NUMBER}_1.fastq -2 ${ACC_NUMBER}_2.fastq -S ${ACC_NUMBER}.sam
 else
    fastqc ${ACC_NUMBER}_1.fastq
 
-   hisat2 --dta -x $INDEX -U ${ACC_NUMBER}_1.fastq -S ${ACC_NUMBER}.sam
+   hisat2 -p $NPROC --dta -x $INDEX -U ${ACC_NUMBER}_1.fastq -S ${ACC_NUMBER}.sam
 fi
 
 ## Generting sorted bam file
-samtools sort -m 2G -o ${ACC_NUMBER}.bam ${ACC_NUMBER}.sam
+samtools sort -@ $NPROC -m 2G -o ${ACC_NUMBER}.bam ${ACC_NUMBER}.sam
 rm ${ACC_NUMBER}.sam
 rm *.fastq
 rm $HOME/ncbi/public/sra/${ACC_NUMBER}.sra
-samtools index ${ACC_NUMBER}.bam
-bamCoverage -bs 10 --normalizeUsing CPM --bam ${ACC_NUMBER}.bam -o ${ACC_NUMBER}.bw
+samtools index -@ $NPROC ${ACC_NUMBER}.bam
+bamCoverage -p $NPROC -bs 10 --normalizeUsing CPM --bam ${ACC_NUMBER}.bam -o ${ACC_NUMBER}.bw
 
 ## Transcript assembly
-stringtie -G $ANNOTATION -o ${ACC_NUMBER}.gtf -l ${ACC_NUMBER} ${ACC_NUMBER}.bam
+stringtie -p $NPROC -G $ANNOTATION -o ${ACC_NUMBER}.gtf -l ${ACC_NUMBER} ${ACC_NUMBER}.bam
 
 ## Preparing merge list file for transcriptome merging
 echo ${SAMPLE_FOLDER}/${ACC_NUMBER}.gtf >> ../../results/merge_list.txt
 
 ## Gene Expression Quantification
-stringtie -e -B -G $ANNOTATION -o ${ACC_NUMBER}.gtf ${ACC_NUMBER}.bam
-
-## Write in blackboard
-echo "SAMPLE " ${ACC_NUMBER} " DONE" >> ../../logs/blackboard.txt
-
-## Count number of line in the blackboard to check the number of processed samples
-PROCESSED_SAMPLES=$(wc -l ../../logs/blackboard.txt | awk '{print $1}')
-
-## Submit scripts for transcriptome merging and differential gene expression 
-if [ ${PROCESSED_SAMPLES} -eq ${NUM_SAMPLES} ]
-then
-   qsub -o ${EXP_FOLDER}/logs/transcriptome_merging $INS/scripts/transcriptome_merging.sh ${EXP_FOLDER} $ANNOTATION
-   Rscript $INS/scripts/DE_analysis.R ${EXP_FOLDER}/samples $CONTROL $EXPERIMENTAL $FOLD_CHANGE $Q_VALUE
-fi
-
+stringtie -p $NPROC -e -B -G $ANNOTATION -o ${ACC_NUMBER}.gtf ${ACC_NUMBER}.bam
