fb94d846eea3424f886fff7e02da7269617f8620
Francisco J. Romero-Campero <fran@us.es>
2021-04-11 08:53:03 +0200

    Added slurm prodessing in chipseq pipeline

---
+++ scripts/chipseq_final_processing.sh
@@ -0,0 +1,115 @@
+#! /bin/bash 
+
+WD=$1
+MAIN_FOLDER=$2
+NUM_REPLICATES=$3
+MODE=$4
+INS_DIR=$5
+MICROALGAE=$6 
+HISTONE=$7 
+TF=$8
+INCLUDED_CONTROL=$9
+NPROC=${10}
+
+echo "------------------------"
+echo "WD=" $WD
+echo "MAIN_FOLDER=" ${MAIN_FOLDER}
+echo "NUM_REPLICATES=" ${NUM_REPLICATES}
+echo "MODE=" $MODE
+echo "INS_DIR=" ${INS_DIR}
+echo "MICRO=" $MICROALGAE
+echo "HISTONE=" $HISTONE
+echo "TF=" $TF
+echo "INCLUDED_CONTROL=" ${INCLUDED_CONTROL}
+echo "NPROC=" $NPROC
+echo "----------------------"
+
+echo ""
+echo "---------------------------------------------"
+echo "---------------------------------------------"
+echo "|| STEP 4: FINAL RESULT REPORT GENERATION. ||"
+echo "---------------------------------------------"
+echo "---------------------------------------------"
+echo ""
+
+if [ ${NUM_REPLICATES} -gt 1 ]
+then
+   cd $WD/${MAIN_FOLDER}/results
+   intersectBed -a  $WD/${MAIN_FOLDER}/replicates/replicate_1/replicate_1_peaks.narrowPeak \
+                -b  $WD/${MAIN_FOLDER}/replicates/replicate_2/replicate_2_peaks.narrowPeak > acum_peaks.narrowPeak
+   bigwigCompare -p $NPROC -b1 $WD/${MAIN_FOLDER}/replicates/replicate_1/chip_1.bw \
+                 -b2 $WD/${MAIN_FOLDER}/replicates/replicate_2/chip_2.bw \
+                 --scaleFactors 1:1 --operation add \
+                 --binSize 5 --outFileName sum_chip.bw \
+                 --outFileFormat bigwig 
+   for  i in `seq 3 ${NUM_REPLICATES}`
+   do
+      intersectBed -a acum_peaks.narrowPeak -b ../replicates/replicate_$i/replicate_${i}_peaks.narrowPeak > acum_peaks_2.narrowPeak
+      rm acum_peaks.narrowPeak
+      mv acum_peaks_2.narrowPeak acum_peaks.narrowPeak
+
+      cp sum_chip.bw cp_sum_chip.bw
+      rm sum_chip.bw
+      bigwigCompare -p $NPROC -b1 ../replicates/replicate_$i/chip_$i.bw -b2 cp_sum_chip.bw \
+                    --scaleFactors 1:1 --operation add \
+                    --binSize 5 --outFileName sum_chip.bw \
+                    --outFileFormat bigwig
+      rm cp_sum_chip.bw 
+
+   done
+   mv acum_peaks.narrowPeak output_peaks.narrowPeak
+
+   ## Add bc dependency
+   SCALEFACTOR=$(echo "1/(2*${NUM_REPLICATES})" | bc -l)
+   bigwigCompare -p $NPROC -b1 sum_chip.bw -b2 sum_chip.bw \
+                 --scaleFactors $SCALEFACTOR:$SCALEFACTOR --operation add \
+                 --binSize 5 --outFileName chip_average.bw \
+                 --outFileFormat bigwig 
+   
+else
+   cp $WD/${MAIN_FOLDER}/replicates/replicate_1_peaks.narrowPeak $WD/${MAIN_FOLDER}/results/output_peaks.narrowPeak
+fi
+
+if [ $MODE == "histone_modification" ]
+then
+   Rscript ${INS_DIR}/scripts/create_Rmd.R $WD/${MAIN_FOLDER}/results/ChIP_seq_analysis_report.Rmd $MICROALGAE $MODE $HISTONE ${NUM_REPLICATES} ${INCLUDED_CONTROL}
+elif [ $MODE == "transcription_factor" ]
+then
+   Rscript ${INS_DIR}/scripts/create_Rmd.R $WD/${MAIN_FOLDER}/results/ChIP_seq_analysis_report.Rmd $MICROALGAE $MODE $TF ${NUM_REPLICATES} ${INCLUDED_CONTROL}
+fi
+
+# Add rmarkdown dependency
+Rscript -e "rmarkdown::render('ChIP_seq_analysis_report.Rmd', 'pdf_document')"
+Rscript -e "rmarkdown::render('ChIP_seq_analysis_report.Rmd', 'html_document')"
+
+echo ""
+echo ""
+echo "**************************************************************************************************************"
+echo "*                                    ANALYSIS DONE!!!                                                        *"
+echo "* OUTPUT REPORTS IN HTML AND PDF FORMATS HAVE BEEN GENERATED IN THE FOLDER $WD/${MAIN_FOLDER}/results"
+echo "*                                  ENJOY YOUR RESULTS!!!                                                     *"
+echo "**************************************************************************************************************"
+echo ""
+echo ""
+echo ""
+echo "                                    ░░░░░░░░░░░░░░░░░░░░░░█████████"
+echo "                                    ░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███"
+echo "                                    ░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███"
+echo "                                    ░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██"
+echo "                                    ░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███"
+echo "                                    ░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██"
+echo "                                    ░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██"
+echo "                                    ░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██"
+echo "                                    ░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██"
+echo "                                    ██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██"
+echo "                                    █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██"
+echo "                                    ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██"
+echo "                                    ░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██"
+echo "                                    ░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█"
+echo "                                    ░░████████████░░░█████████████████"
+echo ""
+echo ""
+echo ""
+
+
+
--- scripts/chipseq_replicate_processing.sh
+++ scripts/chipseq_replicate_processing.sh
@@ -1,26 +1,34 @@
+#! /bin/bash
+
 ## Authors: Francisco J. Romero-Campero
 ##          Ana Belen Romero-Losada
 ## Contact: Francisco J. Romero-Campero - fran@us.es
 
-#| /bin/bash
-
 ## Name input parameters
 DATA=$1
 PAIRED=$2
-REPLICATE_FOLDER=$3
-INDEX=$4
-NUM_REPLICATES=$5
-INS=$6
-CONTROL=$7
-CHIP_LEFT=$8
-CHIP_RIGHT=${9}
-CONTROL_LEFT=${10}
-CONTROL_RIGHT=${11}
-CURRENT_REPLICATE=${12}
-MODE=${13}
+WD=$3
+MAIN_FOLDER=$4
+INDEX=$5
+NUM_REPLICATES=$6
+INS=$7
+CONTROL=$8
+CHIP_LEFT=$9
+CHIP_RIGHT=${10}
+CONTROL_LEFT=${11}
+CONTROL_RIGHT=${12}
+CURRENT_REPLICATE=${13}
+MODE=${14}
+NPROC=${15}
+ARCH=${16}
+MICROALGAE=${17}
+HISTONE=${18}
+TF=${19}
+
+REPLICATE_FOLDER=$WD/${MAIN_FOLDER}/replicates/replicate_${CURRENT_REPLICATE}
 
 ##echo "DATA" $DATA
-##echo "PAIRED" $PAIRED
+echo "PAIRED" $PAIRED
 ##echo "replicate folder" ${REPLICATE_FOLDER}
 ##echo "index" $INDEX
 ##echo "number replicates" ${NUM_REPLICATES}
@@ -30,6 +38,7 @@ MODE=${13}
 ##echo "chip right" ${CHIP_RIGHT}
 ##echo "control left" ${CONTROL_LEFT}
 ##echo "control right" ${CONTROL_RIGHT}
+echo "NPROC=" $NPROC
 
 ## Downloading or copying sample file depending on data source
 ##echo "access " ${REPLICATE_FOLDER}
@@ -47,14 +56,14 @@ then
    echo "*******************************************"
    echo ""
 
-   fastq-dump --split-files ${CHIP_LEFT}
+   fastq-dump --gzip --split-files ${CHIP_LEFT}
    if [ $PAIRED == "FALSE" ]
    then
-      mv ${CHIP_LEFT}_1.fastq chip_1.fastq 
+      mv ${CHIP_LEFT}_1.fastq.gz chip_1.fastq.gz 
    elif [ $PAIRED == "TRUE" ]
    then
-      mv ${CHIP_LEFT}_1.fastq chip_1.fastq
-      mv ${CHIP_LEFT}_2.fastq chip_2.fastq
+      mv ${CHIP_LEFT}_1.fastq.gz chip_1.fastq.gz
+      mv ${CHIP_LEFT}_2.fastq.gz chip_2.fastq.gz
    fi
 
    echo ""
@@ -72,14 +81,14 @@ then
       echo "********************************************"
       echo ""
 
-      fastq-dump --split-files ${CONTROL_LEFT}
+      fastq-dump --gzip --split-files ${CONTROL_LEFT}
       if [ $PAIRED == "FALSE" ]
       then
-         mv ${CONTROL_LEFT}_1.fastq control_1.fastq 
+         mv ${CONTROL_LEFT}_1.fastq.gz control_1.fastq.gz 
       elif [ $PAIRED == "TRUE" ]
       then
-         mv ${CONTROL_LEFT}_1.fastq control_1.fastq
-         mv ${CONTROL_LEFT}_2.fastq control_2.fastq
+         mv ${CONTROL_LEFT}_1.fastq.gz control_1.fastq.gz
+         mv ${CONTROL_LEFT}_2.fastq.gz control_2.fastq.gz
       fi
 
       echo ""
@@ -100,7 +109,6 @@ then
    echo ""
 
    cp ${CHIP_LEFT} chip_1.fastq.gz
-   gunzip chip_1.fastq.gz
    ACC_NUMBER=chip
 
    echo ""
@@ -119,8 +127,7 @@ then
       echo ""
 
       cp ${CONTROL_LEFT} control_1.fastq.gz
-      gunzip control_1.fastq.gz
-
+      
       echo ""
       echo "************************************************"
       echo "* COPYING FASTQ FILES FOR INPUT SAMPLE DONE !! *"
@@ -139,9 +146,7 @@ then
    echo ""
 
    cp ${CHIP_LEFT} chip_1.fastq.gz
-   gunzip chip_1.fastq.gz
    cp ${CHIP_RIGHT} chip_2.fastq.gz
-   gunzip chip_2.fastq.gz
    ACC_NUMBER=chip
 
    echo ""
@@ -160,10 +165,8 @@ then
       echo ""
 
       cp ${CONTROL_LEFT} control_1.fastq.gz
-      gunzip control_1.fastq.gz
       cp ${CONTROL_RIGHT} control_2.fastq.gz
-      gunzip control_2.fastq.gz
-
+      
       echo ""
       echo "************************************************"
       echo "* COPYING FASTQ FILES FOR INPUT SAMPLE DONE !! *"
@@ -180,23 +183,23 @@ echo "********************************************************"
 echo ""
 
 ## Sample quality control and read mapping to reference genome. 
-if [ $PAIRED == "yes" ]
+if [ $PAIRED == "TRUE" ]
 then
-   fastqc chip_1.fastq
-   fastqc chip_2.fastq
-   bowtie2 -x $INDEX -1 chip_1.fastq -2 chip_2.fastq -S raw_chip.sam &> chip_mapping_stats_${CURRENT_REPLICATE}
+   fastqc chip_1.fastq.gz
+   fastqc chip_2.fastq.gz
+   bowtie2 -p $NPROC -x $INDEX -1 chip_1.fastq.gz -2 chip_2.fastq.gz -S raw_chip.sam &> chip_mapping_stats_${CURRENT_REPLICATE}
    if [ $CONTROL == "yes" ]
    then
-      bowtie2 -x $INDEX -1 control_1.fastq -2 control_2.fastq -S raw_control.sam &> control_mapping_stats_${CURRENT_REPLICATE}
+      bowtie2 -p $NPROC -x $INDEX -1 control_1.fastq.gz -2 control_2.fastq.gz -S raw_control.sam &> control_mapping_stats_${CURRENT_REPLICATE}
    fi
 else
-   fastqc chip_1.fastq
-   bowtie2 -x $INDEX -U chip_1.fastq -S raw_chip.sam &> chip_mapping_stats_${CURRENT_REPLICATE}
+   fastqc chip_1.fastq.gz
+   bowtie2 -p $NPROC -x $INDEX -U chip_1.fastq.gz -S raw_chip.sam &> chip_mapping_stats_${CURRENT_REPLICATE}
 
    if [ $CONTROL == "yes" ]
    then
       fastqc control_1.fastq
-      bowtie2 -x $INDEX -U control_1.fastq -S raw_control.sam &> control_mapping_stats_${CURRENT_REPLICATE}
+      bowtie2 -p $NPROC -x $INDEX -U control_1.fastq.gz -S raw_control.sam &> control_mapping_stats_${CURRENT_REPLICATE}
    fi
 fi
 
@@ -213,33 +216,34 @@ echo "************************************************"
 echo ""
 
 ## Duplicates removal and bw generation
-samtools sort -n -m 2G -o raw_chip_sorted.bam raw_chip.sam
-samtools fixmate -m raw_chip_sorted.bam chip_fixmate.bam
-samtools sort -m 2G -o chip_dup_sorted.bam chip_fixmate.bam 
-samtools markdup -r chip_dup_sorted.bam chip.bam
-samtools index chip.bam
-bamCoverage -bs 5 --normalizeUsing CPM --bam chip.bam -o chip_${CURRENT_REPLICATE}.bw
+samtools sort -@ $NPROC -n -m 2G -o raw_chip_sorted.bam raw_chip.sam
+samtools fixmate -@ $NPROC -m raw_chip_sorted.bam chip_fixmate.bam
+samtools sort -@ $NPROC -m 2G -o chip_dup_sorted.bam chip_fixmate.bam 
+samtools markdup -@ $NPROC -r chip_dup_sorted.bam chip.bam
+samtools index -@ $NPROC chip.bam
+bamCoverage -p $NPROC -bs 5 --normalizeUsing CPM --bam chip.bam -o chip_${CURRENT_REPLICATE}.bw
 
 if [ $CONTROL == "yes" ]
 then
-   samtools sort -n -m 2G -o raw_control_sorted.bam raw_control.sam
-   samtools fixmate -m raw_control_sorted.bam control_fixmate.bam
-   samtools sort -m 2G -o control_dup_sorted.bam control_fixmate.bam 
-   samtools markdup -r control_dup_sorted.bam control.bam
-   samtools index control.bam
-   bamCoverage -bs 5 --normalizeUsing CPM --bam control.bam -o control_${CURRENT_REPLICATE}.bw
+   samtools sort -@ $NPROC -n -m 2G -o raw_control_sorted.bam raw_control.sam
+   samtools fixmate -@ $NPROC -m raw_control_sorted.bam control_fixmate.bam
+   samtools sort -@ $NPROC -m 2G -o control_dup_sorted.bam control_fixmate.bam 
+   samtools markdup -@ $NPROC -r control_dup_sorted.bam control.bam
+   samtools index -@ $NPROC control.bam
+   bamCoverage -p $NPROC -bs 5 --normalizeUsing CPM --bam control.bam -o control_${CURRENT_REPLICATE}.bw
 fi
 
 ## Remove files
 rm *.sam
-rm *.fastq
+rm *.fastq*
 rm *fixmate*
 rm *dup_sorted*
+rm raw*
 
 echo ""
-echo "**************************************"
-echo "* BAM AND BIGWIG FILES GENERATED !!! *"
-echo "**************************************"
+echo "*******************************"
+echo "* BIGWIG FILES GENERATED !!! *"
+echo "******************************"
 echo ""
 
 echo ""
@@ -278,8 +282,35 @@ else
    exit
 fi
 
+rm *.bam*
+
 echo ""
 echo "************************"
 echo "* PEAK CALLING DONE !!!*"
 echo "************************"
 echo ""
+
+if [ $ARCH == "SLURM" ]
+then
+   ## Write in blackboard
+   echo "REPLICATE " ${CURRENT_REPLICATE} " DONE" >> $WD/${MAIN_FOLDER}/logs/blackboard.txt
+
+   ## Count number of line in the blackboard to check the number of processed samples
+   PROCESSED_REPLICATES=$(wc -l $WD/${MAIN_FOLDER}/logs/blackboard.txt | awk '{print $1}')
+
+   ## Submit scripts for transcriptome merging and differential gene expression 
+   if [ ${PROCESSED_REPLICATES} -eq ${NUM_REPLICATES} ]
+   then
+      if [ $MODE == "histone_modification" ]
+      then
+         sbatch $INS/scripts/chipseq_final_processing.sh $WD ${MAIN_FOLDER} ${NUM_REPLICATES} \
+                                                          $MODE ${INS} $MICROALGAE $HISTONE $HISTONE \
+                                                          $CONTROL $NPROC
+      elif [ $MODE == "transcription_factor" ]
+      then
+         sbatch $INS/scripts/chipseq_final_processing.sh $WD ${MAIN_FOLDER} ${NUM_REPLICATES} \
+                                                          $MODE ${INS} $MICROALGAE $TF $TF \
+                                                          $CONTROL $NPROC
+      fi
+   fi
+fi
