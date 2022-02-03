6c7875d81581d851857188d45f85cdd9400c57ee
Ana Belén Romero-Losada <arlosada@us.es>
2022-02-03 15:44:33 +0100

    Update test_kallisto_DE.R

--- scripts/test_kallisto_DE.R
+++ scripts/test_kallisto_DE.R
@@ -13,8 +13,16 @@ mapper <- "kallisto"
 num_samples <- 2
 setwd(working.directory)
 
-kallisto_sample1 <- read.table(file="../opt/kallisto_out_sample1/abundance.tsv",header=T)
-kallisto_sample2 <- read.table(file="../opt/kallisto_out_sample2/abundance.tsv",header=T)
+gene.expression <- data.frame()
+i<-1
+for (i in 1:num_samples)
+{
+  current_sample <- paste(working.directory,"opt/kallisto_out_sample_", i, "/abundance.tsv", sep = "")
+  #cambiar la ruta de opt a la carpeta de la muestra "samples/sample_", i, "/kallisto_out/abundance.tsv"
+  kallisto_current_sample <- read.table(file="../opt/kallisto_out_sample1/abundance.tsv",header=T)
+}
+kallisto_sample_1 <- read.table(file="../opt/kallisto_out_sample1/abundance.tsv",header=T)
+kallisto_sample_2 <- read.table(file="../opt/kallisto_out_sample2/abundance.tsv",header=T)
 
-gene.expression <- data.frame(kallisto_sample1[,1], kallisto_sample1[,5], kallisto_sample2[,5])
+gene.expression <- data.frame(kallisto_sample_1[,1], kallisto_sample_1[,5], kallisto_sample_2[,5])
 colnames(gene.expression) <- c("Transcripts ID", "sample_1", "sample_2")
